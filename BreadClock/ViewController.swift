2dd279538595110f6c5892541ae521dbf267d149
emilymalecbrown <emilymalecbrown@gmail.com>
2018-05-19 10:28:21 -0400

    Initial Commit

---
+++ BreadClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BreadClock
+//
+//  Created by Emily Brown on 5/19/18.
+//  Copyright © 2018 Emily Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
